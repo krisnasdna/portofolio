6ef492f350ac202628128807b4f2f8cedba13856
krisnasdna <krisnasedana2403@gmail.com>
2025-07-17 20:46:23 +0800

    update animation again

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="mx-5 lg:mx-[80px] my-0">
     <RevealText class="text-[42px] lg:text-[230px] font-[900] text-[#334EAC] uppercase leading-none ">Want to know more?</RevealText>
-    <h2 class="lg:text-[28px] text-base font-semibold uppercase text-center lg:mt-60 mt-20 reveal-on-scroll">Let me take you behind the code.</h2>
+    <h2 class="lg:text-[28px] text-base font-semibold uppercase text-center lg:mt-60 mt-20 reveal">Let me take you behind the code.</h2>
     <div class="grid lg:grid-cols-2 grid-cols-1 justify-start lg:mt-60 mt-20">
       <div class="justify-self-start row-start-2 lg:row-start-auto mt-16">
         <img src="~/assets/image/karakter.gif" alt="icon">
       </div>
-      <div class="flex flex-col gap-10">
-          <h2 class="lg:text-[28px] text-base italic font-medium text-[#334EAC] text-justify lg:leading-[1.2] inline" ref="animatedText1">Hi, I’m Krisna — a tech enthusiast with a passion for front-end development and web  design. I recently graduated with a degree in Information Technology, and now I spend most of my time turning ideas into engaging web experiences.</h2>
-          <h2 class="lg:text-[28px] text-base italic font-medium text-[#334EAC] text-justify lg:leading-[1.2] inline" ref="animatedText2">Outside of design and development, I’m also exploring about  AI and IoT — because I believe the future of the web goes far beyond just screens. I’m always learning, always building, and always up for a challenge.</h2>
+      <div class="flex flex-col gap-10 w-full ">
+          <h2 class="lg:text-[28px] text-base italic font-medium text-[#334EAC] text-justify lg:leading-[1.2]" ref="animatedText1">Hi, I’m Krisna — a tech enthusiast with a passion for front-end development and web  design. I recently graduated with a degree in Information Technology, and now I spend most of my time turning ideas into engaging web experiences.</h2>
+          <h2 class="lg:text-[28px] text-base italic font-medium text-[#334EAC] text-justify lg:leading-[1.2]" ref="animatedText2">Outside of design and development, I’m also exploring about  AI and IoT — because I believe the future of the web goes far beyond just screens. I’m always learning, always building, and always up for a challenge.</h2>
       </div>
     </div>
   </div>
@@ -32,47 +32,46 @@ const animateTextSequentially = () => {
     $ScrollTrigger.getAll().forEach(trigger => trigger.kill())
 
     const split1 = $SplitText.create(animatedText1.value, {
-      type: 'lines',
-      linesClass: 'line',
-      mask: 'lines',
+      type: 'words,lines',
+      linesClass: 'span-words',
+      mask: 'words',
     })
 
     const split2 = $SplitText.create(animatedText2.value, {
-      type: 'lines',
-      linesClass: 'line',
-      mask: 'lines',
+      type: 'words,lines',
+      linesClass: 'span-words',
+      mask: 'words',
     })
 
     const tl = $gsap.timeline({
       scrollTrigger: {
         trigger: animatedText1.value,
         start: 'top 90%',
-        toggleActions: 'restart none none none',
-        once: false,
+        toggleActions: 'play none none none',
       },
     })
 
-    tl.from(split1.lines, {
+    tl.from(split1.words, {
+      autoAlpha: 0,
       yPercent: 100,
       opacity: 0,
-      stagger: 0.1,
       duration: 1,
-      ease: 'expo.out',
+      ease: 'circ.out',
     })
-      .to(split1.lines, { opacity: 1 }, '<')
+      .to(split1.words, { opacity: 1 }, '<')
       .from(
-        split2.lines,
+        split2.words,
         {
           yPercent: 100,
           opacity: 0,
-          stagger: 0.1,
           duration: 1,
-          ease: 'expo.out',
+          ease: 'circ.out',
         },
-        '+=0.2' // jeda antar animasi
+        '-=0.1' 
       )
   })
 }
+
 
 onMounted(async () => {
   await nextTick()
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-5 lg:mx-[80px] my-0 lg:overflow-hidden ">
-    <RevealText class="text-[68px] lg:text-[230px] font-[900] text-[#334EAC] uppercase leading-none text-center">Krisna Sedana</RevealText>
+    <RevealText class="text-[80px] lg:text-[230px] font-[900] text-[#334EAC] uppercase leading-none text-center">Krisna Sedana</RevealText>
     <div class="grid lg:grid-cols-3 grid-cols-1 justify-center items-center h-full mt-10 ">
       <div class="flex flex-col justify-center items-center text-center gap-10 reveal">
         <div >
